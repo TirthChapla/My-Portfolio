3879da050a563800bd1c92a7495a98a22c11218f
TirthChapla <tirthchapla123@gmail.com>
2025-03-17 18:41:08 +0530

    problem in build it tell that app.module.css is not correctly imported so modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import styles from "./app.module.css"
+import styles from "./App.module.css"
+
 import { Navbar } from "./components/Navbar/Navbar"
 import { Hero } from "./components/Hero/Hero"
 import { About } from "./components/About/About"
