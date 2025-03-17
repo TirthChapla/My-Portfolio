2431dcca9aedfc0ea46b429f8264046f985ec119
TirthChapla <tirthchapla123@gmail.com>
2025-03-17 18:45:01 +0530

    problem in build it tell that skill.json & history.json is not correctly imported so modified

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Experience.module.css";
-import skills from "../../data/skills.json";
-import history from "../../data/history.json";
+import skills from "../../Data/skills.json";
+import history from "../../Data/history.json";
 
 
 export const Experience = () => {
